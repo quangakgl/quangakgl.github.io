22dffaae6dde9e116f2a845dec1688ae0df4316b
quangakgl <quangprolathe@gmail.com>
2017-04-02 23:12:18 +0700

    a
    cap nhat

--- Project/js/quang.js
+++ Project/js/quang.js
@@ -1,13 +1,12 @@
 $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('#btn-to-top').css('display', 'block');
-        } else {
-            $('#btn-to-top').css('display', 'none');
-        }
-        ;
-    });
-    $('#btn-to-top').click(function () {
-        $('html,body').animate({
-            scrollTop: 0
-        }, 1000);
-    });
+    if ($(this).scrollTop() > 100) {
+        $('#btn-to-top').css('display', 'block');
+    } else {
+        $('#btn-to-top').css('display', 'none');
+    };
+});
+$('#btn-to-top').click(function () {
+    $('html,body').animate({
+        scrollTop: 0
+    }, 1000);
+});
